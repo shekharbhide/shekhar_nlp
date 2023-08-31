625efac5e522e4353d8e73e258b3e817accd14e9
Shekhar Bhide <shekharbhide036@gmail.com>
2023-08-31 16:24:17 +0530

    ass1

--- 01.py
+++ 01.py
@@ -1,2 +1,30 @@
-print('hkkkkkkkkkkkkkkk')
-print('helffflo')
+import spacy
+nlp = spacy.load("en_core_web_sm")
+about_text = (
+   "Good better best"
+     "Never let it rest"
+     "Till your good is better "
+     " And your better best."
+ )
+about_doc = nlp(about_text)
+
+for token in about_doc:
+  print (token, token.idx)
+  
+
+
+spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
+len(spacy_stopwords)
+
+for stop_word in list(spacy_stopwords)[:10]:
+    print(stop_word)
+    custom_about_text = (
+    "Good better best"
+     "Never let it rest"
+     "Till your good is better "
+     " And your better best."
+)
+nlp = spacy.load("en_core_web_sm")
+about_doc = nlp(custom_about_text)
+print([token for token in about_doc if not token.is_stop])
+
