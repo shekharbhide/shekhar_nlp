2ac7f7aa063870c99b5127f1412beebeb9d00e96
shekharbhide <shekharbhide036@gmail.com>
2023-10-05 12:16:46 +0530

    2nd asg

--- 02_asg_2.py
+++ 02_asg_2.py
@@ -22,4 +22,3 @@ print(id_words)
 
 
 
-# git remote set-url origin https://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@github.com/shekharbhide/shekhar_nlp.git
---
+++ cmd
@@ -0,0 +1,1 @@
+git remote set-url origin https://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@github.com/shekharbhide/shekhar_nlp.git
